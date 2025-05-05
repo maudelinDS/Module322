43e89acad726c3823aae51fac1b9a6d5cd83d926
Mds <maudelin.delescaut@jobtrek.ch>
2025-05-05 18:16:31 +0200

    update login

---
+++ src/app/Pages/favoris/favoris.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favoris',
+  imports: [],
+  templateUrl: './favoris.component.html',
+  styleUrl: './favoris.component.css'
+})
+export class FavorisComponent {
+
+}
